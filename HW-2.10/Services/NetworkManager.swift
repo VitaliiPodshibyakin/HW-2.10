763a05ba33f21b86663603bdd27d3433ab7d2144
VitaliiPodshibyakin <vitalikpod@yandex.ru>
2022-03-30 22:13:36 +0300

    nom

--- HW-2.10/Services/NetworkManager.swift
+++ HW-2.10/Services/NetworkManager.swift
@@ -5,4 +5,31 @@
 //  Created by Виталий Подшибякин on 30.03.2022.
 //
 
-import Foundation
+//protocol NetworkManagerDelegate {
+//    func updateUI(_: NetworkManager, with anime: Anime)
+//}
+//import Foundation
+//
+//
+//class NetworkManager {
+//
+//    var delegate: NetworkManagerDelegate?
+//    var animes: [Anime] = []
+//
+//    func fetch() {
+//        let urlString = "https://ghibliapi.herokuapp.com/films"
+//        guard let url = URL(string: urlString) else { return }
+//
+//        URLSession.shared.dataTask(with: url) { data, _, error in
+//            guard let data = data else {
+//                print(error?.localizedDescription ?? "No error description")
+//                return
+//            }
+//        do {
+//            self.animes = try JSONDecoder().decode([Anime].self, from: data)
+//        } catch let error {
+//            print(error.localizedDescription)
+//        }
+//        }.resume()
+//}
+//}
